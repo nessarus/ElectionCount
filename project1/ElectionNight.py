997f38398f9e4a2eea4992dac969efdcd970c1f1
Joshua Ng <31525854+nessarus@users.noreply.github.com>
2023-01-10 23:14:15 +1100

    Clean up.

--- project1/ElectionNight.py
+++ project1/ElectionNight.py
@@ -1,176 +1,187 @@
-##  Name:               Joshua Ng
-##  Student Number:     20163079
-##  Version:            15/4/2017
-
-
+"""
+Name:               Joshua Ng
+Student Number:     20163079
+Version:            31/12/2022
+"""
 import os.path
 
-## 1. getCandidates(f) returns a list containing the candidates’ names from the file f.
-## The names will be one per line with no extraneous characters. Disregard any blank
-## lines in f. If f doesn’t exist, print an appropriate error message and return the 
-## empty list.
-## For example, getCandidates("candidates.txt") = ["Major Clanger", "Soup Dragon",
-## "Froglet", "Iron Chicken", "The Cloud"]
-def getCandidates(f): #getCandidates(f): >> ["Major Clanger", "...], >> print(error); []
-    candidates = []
-    if os.path.isfile(f):
-        file_f = open(f, "r")
-        for names in file_f:
-            if names not in [’\n’, ’\r\n’]:
-                candidates.append(names.strip())
-        file_f.close()
-    else:
-        print("file doesn’t exist")
-    return candidates
+
+def get_candidates(candidates_filename):
+    """
+    Gets a list containing the candidates' names from the file.
+    Candidate file format:
+    * If f doesn't exist an error message is printed.
+    * One name per line.
+    * No extraneous characters.
+    * Disregard blank lines.
+    :param candidates_filename: The file name containing the candidate names.
+    :return:                    A list of candidate names.
+    """
+    if os.path.isfile(candidates_filename):
+        with open(candidates_filename, "r") as file_f:
+            return [name.strip() for name in file_f]
+
+    print("File does not exist.")
+    return []
 
 
-# 2. parseVote(s) returns the vote from s: parseVote("15") = parseVote(" 15 ") = 15.
-# return 0 for an empty vote i.e. parseVote("") = parseVote(" ") = 0,
-# -1 if there are any non-digits 
-# i.e. parseVote("-3") = parseVote("no") = parseVote("1 5") = -1
-def parseVote(s): #parseVote(s): " " >> 0, "no" >> -1, "9" >> 9
-    vote = s.strip()
-    if vote == "":
+def parse_vote(token):
+    """
+    Parses the numbered preferences.
+
+    * Empty votes return 0. i.e. " ", "".
+    * Non-digits return -1. i.e. "-3", "1 5" "not digits"
+    :param token:   The string token to parse.
+    :return:        An integer of the parsed vote.
+    """
+    token = token.strip()
+
+    if not token:
         return 0
-    elif not vote.isdigit():
+
+    if not token.isdigit():
         return -1
-    else:
-        return int(s)
+
+    return int(token)
 
 
-# 3. parsePaper(s, n) returns the votes from the ballot paper s in an
-# election with n candidates, plus an error message if appropriate.
-# If s is formal, return the list of numbers found in s and the empty string.
-# i.e. parsePaper("14, , 2", 4) = ([14, 0, 2], "")
-# If s is informal, return an empty list of numbers and the appropriate string below.
-# i.e. parsePaper(", , ", 4) = parsePaper("0, 0", 4) = ([ ], "blank"),
-# i.e. parsePaper("4, -8, 0", 4) = parsePaper("4, 7.8, 0", 4) = parsePaper("pointless,
-# 5, 5", 4) = ([ ], "non-digits"),
-# i.e. parsePaper("1,2,,4,5", 4) = ([ ], "too long").
-# parsePaper(s,n): >> ([14, 0, 2], ""), >> ([ ], "blank"), 
-#                  >> ([ ], "non-digits"), >> ([], "too long")
-def parsePaper(s, n):
-    ballot = s.split(",")
-    len_ballot = len(ballot)
-    votes = []
-    blank_vote = 0
+def parse_paper(paper, number_of_candidates):
+    """
+    Parses a ballot paper's numbered preferences.
 
-    if len_ballot > n:
-        return ([], "too long")
+    Ballot paper's string format:
+    * Comma separated integers
+    * E.g. "14, 0, 2", "1, 2,, 4, 5".
 
-    for vote in ballot:
-        parse_vote = parseVote(vote) #parseVote(s): " " >> 0, "no" >> -1, "9" >> 9
-        if parse_vote == -1:
-            return ([], "non-digits")
-        elif parse_vote == 0:
-            blank_vote+=1
-            votes.append(0)
-        else:
-            votes.append(parse_vote)
+    Error Messages:
+    * "too long":   Number of preferences exceeds number of candidates.
+    * "blank":      All zero or blank preferences.  E.g. ", , ", "0, 0", "0, "
+    * "non-digits"  Use of negative or non-digits.  E.g. "pointless, 5, 5"
 
-    if blank_vote == len_ballot:
-        return ([], "blank")
-    return (votes, "")
+    :param paper:                   A string with the numbered preferences to be parsed.
+    :param number_of_candidates:    The number of candidates.
+    :return: If successful returns tuple of parsed ranked choices list and empty string.
+             Otherwise, returns tuple of an empty list with an error message.
+    """
+    votes = [parse_vote(token) for token in paper.split(",")]
 
-## 4. getPapers(f, n) returns a list containing the ballot papers from the file f, in an
-## election with n candidates. Treat each line of the file as a separate paper. If f 
-## doesn’t exist, print an appropriate error message and return the empty list. For 
-## example:
-## getPapers("smallfile.txt", 4) = [
-##      ([1, 2, 3, 4], ""), ([], "blank"), ([0, 23, 0], ""),
-##      ([], "non-digits"), ([], "non-digits"), ([4, 0, 4, 4], ""),
-##      ([], "too long"), ([], "blank")
-## ]
-def getPapers(f, n):
-    papers = []
-    if os.path.isfile(f):
-        file_f = open(f, "r")
-        for line in file_f:
-            papers.append(parsePaper(line,n))
-            #parsePaper(s,n): >> ([14, 0, 2], ""), >> ([ ], "blank"),
-            #                 >> ([ ], "non-digits"), >> ([ ], "too long")
-        file_f.close()
-    else:
-        print("file doesn’t exist")
+    if len(votes) > number_of_candidates:
+        return [], "too long"
+
+    if any(vote == -1 for vote in votes):
+        return [], "non-digits"
+
+    if all(vote == 0 for vote in votes):
+        return [], "blank"
+
+    return votes, ""
+
+
+def get_papers(ballots_file, number_of_candidates):
+    """
+    Gets the ballot papers parsed from the file.
+
+    Ballot paper format:
+    * Each line is a separate ballot paper.
+
+    :param ballots_file:            The file name of the file containing the ballot papers.
+    :param number_of_candidates:    The number of candidates.
+    :return:    A list of tuples with the parsed votes and its error status.
+                If the file not found, an empty list is returned.
+    """
+    if os.path.isfile(ballots_file):
+        with open(ballots_file, "r") as file_f:
+            return [parse_paper(paper, number_of_candidates) for paper in file_f]
+
+    print("File does not exist.")
+    return []
+
+
+def normalise_paper(paper, number_of_candidates):
+    """
+    Rescales the choice preferences to be normalised to a total of 1.
+    List is padded with zeros in line with the number of candidates.
+    For example:
+    * normalise_paper([1,2,3,4], 4)     := [0.1, 0.2, 0.3, 0.4],
+    * normalise_paper([2], 3)           := [1.0, 0.0, 0.0],
+    * normalise_paper([0, 4, 496], 3)   := [0.000, 0.008, 0.992]
+    :param paper:                   A string with the numbered preferences to be parsed.
+    :param number_of_candidates:    The number of candidates.
+    :return:    A list of normalised choice preferences.
+    """
+    total = sum(paper)
+    papers = [float(vote)/total for vote in paper]
+
+    for i in range(len(papers), number_of_candidates):
+        papers.append(0.0)
+
     return papers
 
-## 5. normalisePaper(p, n) returns p with each vote scaled according to its total,
-## and padded to contain n votes.
-## For example:
-## normalisePaper([1,2,3,4], 4) = [0.1, 0.2, 0.3, 0.4],
-## normalisePaper([2], 3) = [1.0, 0.0, 0.0],
-## normalisePaper([0, 4, 496], 3) = [0.000, 0.008, 0.992]
-def normalisePaper(p, n): # sum(p) > 0
-    paper = []
-    sum_p = sum(p)
 
-    for vote in p:
-        paper.append(float(vote)/sum_p)
-    for i in range(len(paper), n):
-        paper.append(0.0)
-    return paper
+def normalise_papers(papers, number_of_candidates):
+    """
+    Normalises a list of ballot papers.
+    E.g. normalise_papers([[2], [7, 2, 1]], 3) = [[1.0, 0.0, 0.0], [0.7, 0.2, 0.1]].
+    :param papers:                  The ballot papers to be normalises.
+    :param number_of_candidates:    The number of candidates.
+    :return:    A list of normalised ballot papers.
+    """
+    return [normalise_paper(paper, number_of_candidates) for paper in papers]
 
-## 6. normalisePapers(ps, n) returns ps with each paper normalised, in an election with n
-## candidates.
-## e.g. normalisePapers([[2], [7, 2, 1]], 3) = [[1.0, 0.0, 0.0], [0.7, 0.2, 0.1]].
-## normalisePapers will use normalisePaper.
-def normalisePapers(ps, n): # for every p on ps, sum(p) > 0
-    papers = []
-    for p in ps:
-        papers.append(normalisePaper(p,n)) #normalisePaper(p,n): >> [0.1, 0.2, 0.3, 0.4]
-    return papers
 
-## 7. countVotes(cs, ps) returns a list of lists containing the counts for the candidates
-## cs from the ballot papers ps, in descending order of total number of votes.
-## For example, countVotes(["A", "B", "C"], [[0.5, 0.5, 0], [0.05, 0.3, 0.65]])
-## = [[0.8, "B"], [0.65, "C"], [0.55, "A"]]. 
-def countVotes(cs, ps): # ps have been normalised for an election with len(cs) candidates
-    results = []
-    for candidate in range(len(cs)):
-        sum = 0
-        for vote in ps:
-            sum += vote[candidate]
-        results.append([sum, cs[candidate]])
+def count_votes(candidates, papers):
+    """
+    Counts the ballot papers for each candidate. A list of lists containing the counts for the candidates
+    cs from the ballot papers ps, in descending order of total number of votes.
+    For example, count_votes(["A", "B", "C"], [[0.5, 0.5, 0], [0.05, 0.3, 0.65]])
+    = [[0.8, "B"], [0.65, "C"], [0.55, "A"]].
+    :param candidates:  A list of candidates.
+    :param papers:      A list of ballot papers.
+    :return:    A list of candidates with their total number of votes.
+    """
+    results = [[sum(votes), candidates[index]] for index, votes in enumerate(list(zip(*papers)))]
     results.sort(reverse=True)
     return results
 
-## 8. printCount(c) displays the election count c, i.e. the result from countVotes.
-def printCount(c):
-    for i in range(len(c)):
-        print(" {0:0.2f} {1}".format(c[i][0], c[i][1]))
 
-## 9. main() prompts the user for the names of the necessary files, then conducts the 
-## election.
-## main() will use getCandidates, getPapers, normalisePapers, countVotes, and 
-## printCount.
+def print_count(results):
+    """
+    Displays the election count results.
+    :param results:     The election count results.
+    """
+    for candidate in results:
+        print(" {0:0.2f} {1}".format(candidate[0], candidate[1]))
+
+
 def main():
-    candidates_file = str(input("Please enter the candidates’ filename: "))
-    candidates = getCandidates(candidates_file)
-    if candidates == []:
+    """
+    Prompts the user for the names of the necessary files, then conducts the
+    election.
+    """
+    candidates_file = str(input("Please enter the candidates’ filename: \n"))
+    candidates = get_candidates(candidates_file)
+
+    if not candidates:
         return
-    number_candidates = len(candidates)
-    
-    papers_file = str(input("Please enter the ballot papers’ filename: "))
-    papers = getPapers(papers_file, number_candidates)
-    if papers == []:
-        return
-    
-    formal_papers = []
-    for p in papers:
-        if not p[0] == []:
-            formal_papers.append(p[0])
-    
-    normal_papers = normalisePapers(formal_papers, number_candidates)
-    results = countVotes(candidates, normal_papers)
-    
+
+    number_of_candidates = len(candidates)
+
+    papers_file = str(input("Please enter the ballot papers’ filename: \n"))
+    papers = get_papers(papers_file, number_of_candidates)
+    formal_papers = [paper for paper, error in papers if not error]
+
+    normal_papers = normalise_papers(formal_papers, number_of_candidates)
+    results = count_votes(candidates, normal_papers)
+
     number_formal = len(formal_papers)
     number_informal = len(papers) - number_formal
 
+
     print("Nerdvanian election 2017\n")
-    print("There were {0} informal votes".format(number_informal))
-    print("There were {0} formal votes\n".format(number_formal))
-    printCount(results)
+    print("There were {} informal votes".format(number_informal))
+    print("There were {} formal votes\n".format(number_formal))
+    print_count(results)
 
 
-
-        
+if __name__ == "__main__":
+    main()
--- project1/project1test.py
+++ project1/project1test.py
@@ -10,14 +10,13 @@ Author: Lyndon While
 Date: 3/4/14
 Version 1.15 
 """
-
-
-import project1
+import ElectionNight as Project1
 import os.path
 
 
-#a non-existent file
+# A non-existent file
 nonfile = "nofile.txt"
+
 
 def hackb(xs):
     if xs == [] or xs[-1][1] != "blank":
@@ -26,58 +25,66 @@ def hackb(xs):
         return xs[:-1]
 
 
-def test_getCandidates():
+def test_get_candidates():
     f = "candidates.txt"
     if os.path.exists(f):
-        return [(project1.getCandidates(x), y) for (x, y) in 
+        return [(Project1.get_candidates(x), y) for (x, y) in
                 [(f, ["Major Clanger", "Soup Dragon", "Froglet", "Iron Chicken", "The Cloud"])
-                ,(nonfile, [])]]
+                , (nonfile, [])]]
     else:
-        print("You need to download file", f, end = "\n\n")
+        print("You need to download file", f, end="\n\n")
         return [] 
 
-def test_parseVote():
-    return [(project1.parseVote(x), y) for (x, y) in
+
+def test_parse_vote():
+    return [(Project1.parse_vote(x), y) for (x, y) in
             [("", 0), ("no", -1), ("27", 27)]]
 
-def test_parsePaper():
-    return [(project1.parsePaper(x, y), z) for (x, y, z) in
+
+def test_parse_paper():
+    return [(Project1.parse_paper(x, y), z) for (x, y, z) in
             [(",,, ",      4, ([],         "blank"))
             ,("  3,4,5  ", 2, ([],         "too long"))
             ,("4,-4,4",    3, ([],         "non-digits"))
             ,("4,5, 16",   6, ([4, 5, 16], ""))
             ,("9876",      8, ([9876],     ""))]]
 
-def test_getPapers():
+
+def test_get_papers():
     f = "smallfile.txt"
     if os.path.exists(f):
         nd = ([], "non-digits")
         b  = ([], "blank")
         tl = ([], "too long")
-        return [(hackb(project1.getPapers(x, 4)), y) for (x, y) in
+        return [(hackb(Project1.get_papers(x, 4)), y) for (x, y) in
                 [(f, [([1,2,3,4], ""), b, ([0,23,0], ""), nd, nd, ([4,0,4,4], ""), tl])
                 ,(nonfile, [])]]
     else:
         print("You need to download file", f, end = "\n\n")
         return []
 
-def test_normalisePaper():
-    return [(project1.normalisePaper(x, y), z) for (x, y, z) in
+
+def test_normalise_paper():
+    return [(Project1.normalise_paper(x, y), z) for (x, y, z) in
             [([2,1,5], 3, [0.25, 0.125, 0.625])
             ,([8,2],   4, [0.8,  0.2,   0,    0])
             ,([7],     4, [1,    0,     0,    0])]]
 
-def test_normalisePapers():
-    return [(project1.normalisePapers(x, y), z) for (x, y, z) in
+
+def test_normalise_papers():
+    return [(Project1.normalise_papers(x, y), z) for (x, y, z) in
             [([[2,1,5], [8,2]], 3, [[0.25,0.125,0.625], [0.8,0.2,0]])]]
 
-def test_countVotes():
-    return [(project1.countVotes(cs, ps), x) for (cs, ps, x) in
+
+def test_count_votes():
+    return [(Project1.count_votes(cs, ps), x) for (cs, ps, x) in
             [(["A", "B", "C"], [[0,0,1], [0,1,0], [0,0,1]], [[2,   "C"], [1,   "B"], [0,   "A"]])
             ,(["A", "B", "C"], [[0.7, 0.2, 0.1]],           [[0.7, "A"], [0.2, "B"], [0.1, "C"]])]]
 
-def test_printCount():
+
+def test_print_count():
     return []
+
 
 def test_main():
     return []
@@ -94,14 +101,15 @@ def msg(f, z):
         s = "These tests incorrect: " + str(zs)[1:-1]
     print("%20s" % (f + ":"), s)
 
-msg("getCandidates",      test_getCandidates())
-msg("parseVote",          test_parseVote())
-msg("parsePaper",         test_parsePaper())
-msg("getPapers",          test_getPapers())
-msg("normalisePaper",     test_normalisePaper())
-msg("normalisePapers",    test_normalisePapers())
-msg("countVotes",         test_countVotes())
-msg("printCount",         test_printCount())
-msg("main",               test_main())
+
+msg("get_candidates",   test_get_candidates())
+msg("parse_vote",       test_parse_vote())
+msg("parse_paper",      test_parse_paper())
+msg("get_papers",       test_get_papers())
+msg("normalise_paper",  test_normalise_paper())
+msg("normalise_papers", test_normalise_papers())
+msg("count_votes",      test_count_votes())
+msg("print_count",      test_print_count())
+msg("main",             test_main())
 print()
 input("Hit Enter to finish: ")
